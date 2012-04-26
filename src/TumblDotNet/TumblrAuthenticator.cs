48d0e978286a04efed2802cb960aa5fa00ba88c2
Richard Knight <richard.knight@bathasu.com>
2012-04-26 11:27:43 +0100

    refactored oauth flow out to seperate class

---
+++ src/TumblDotNet/TumblrAuthenticator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+using RestSharp.Authenticators;
+using TumblDotNet.Authentication;
+
+namespace TumblDotNet
+{
+    public static class TumblrAuthenticator
+    {
+
+        private const string REQUEST_TOKEN_URL = "http://www.tumblr.com/oauth/request_token";
+        private const string AUTHORIZE_URL = "http://www.tumblr.com/oauth/authorize";
+        private const string ACCESS_TOKEN_URL = "http://www.tumblr.com/oauth/access_token";
+
+        #region OAuth Setup
+
+        public static Token GetRequestToken(string consumerKey, string consumerSecret)
+        {
+            var client = new RestClient();
+            client.Authenticator = OAuth1Authenticator.ForRequestToken(consumerKey, consumerSecret);
+            var request = new RestRequest(REQUEST_TOKEN_URL, Method.GET);
+            var response = client.Execute(request);
+            return GetTokenFromParams(response.Content);
+        }
+
+        public static String BuildAuthorizeUrl(Token requestToken)
+        {
+            return AUTHORIZE_URL + "?oauth_token=" + requestToken.UserToken;
+        }
+
+        //TODO: Un-steal this
+
+        private static Token GetTokenFromParams(string urlParams)
+        {
+
+            string secret = "";
+            string token = "";
+            //TODO - Make this not suck
+            var parameters = urlParams.Split('&');
+
+            foreach (var parameter in parameters)
+            {
+                if (parameter.Split('=')[0] == "oauth_token_secret")
+                {
+                    secret = parameter.Split('=')[1];
+                }
+                else if (parameter.Split('=')[0] == "oauth_token")
+                {
+                    token = parameter.Split('=')[1];
+                }
+            }
+
+            return new Token(token, secret);
+        }
+
+        public static Token GetAccessToken(string consumerKey, string consumerSecret, Token requestToken, string verifier)
+        {
+            var client = new RestClient();
+
+            if (String.IsNullOrEmpty(verifier))
+                throw new ArgumentException("Invalid verifier");
+
+            client.Authenticator = OAuth1Authenticator.ForAccessToken(consumerKey, consumerSecret, requestToken.UserToken, requestToken.UserSecret, verifier);
+
+
+            var request = new RestRequest(ACCESS_TOKEN_URL, Method.GET);
+            var response = client.Execute(request);
+
+            return GetTokenFromParams(response.Content);
+
+        }
+
+        #endregion
+    }
+}
--- src/TumblDotNet/TumblrClient.cs
+++ src/TumblDotNet/TumblrClient.cs
@@ -12,9 +12,6 @@ namespace TumblDotNet
     public class TumblrClient
     {
         private const string API_BASE = "http://api.tumblr.com/v2";
-        private const string REQUEST_TOKEN_URL = "http://www.tumblr.com/oauth/request_token";
-        private const string AUTHORIZE_URL = "http://www.tumblr.com/oauth/authorize";
-        private const string ACCESS_TOKEN_URL = "http://www.tumblr.com/oauth/access_token";
 
         #region Tokens
 
@@ -63,66 +60,6 @@ namespace TumblDotNet
         public TumblrClient(string consumerKey, string consumerSecret, Token accessToken) : this(consumerKey, consumerSecret, accessToken.UserToken, accessToken.UserSecret)
         {}
         
-        #endregion
-
-        #region OAuth Setup
-
-        public static Token GetRequestToken(string consumerKey, string consumerSecret)
-        {
-            var client = new RestClient();
-            client.Authenticator = OAuth1Authenticator.ForRequestToken(consumerKey, consumerSecret);
-            var request = new RestRequest(REQUEST_TOKEN_URL,Method.GET);
-            var response = client.Execute(request);
-
-            return GetTokenFromParams(response.Content);
-        }
-
-        public static String BuildAuthorizeUrl(Token requestToken)
-        {
-            return "http://www.tumblr.com/oauth/authorize?oauth_token=" + requestToken.UserToken;
-        }
-
-        //TODO: Un-steal this
-        private static Token GetTokenFromParams(string urlParams)
-        {
-
-            string secret = "";
-            string token = "";
-            //TODO - Make this not suck
-            var parameters = urlParams.Split('&');
-
-            foreach (var parameter in parameters)
-            {
-                if (parameter.Split('=')[0] == "oauth_token_secret")
-                {
-                    secret = parameter.Split('=')[1];
-                }
-                else if (parameter.Split('=')[0] == "oauth_token")
-                {
-                    token = parameter.Split('=')[1];
-                }
-            }
-
-            return new Token(token,secret);
-        }
-
-        public static Token GetAccessToken(string consumerKey, string consumerSecret, Token requestToken, string verifier)
-        {
-            var client = new RestClient();
-
-            if(string.IsNullOrEmpty(verifier))
-                throw new ArgumentException("Invalid verifier");
-
-            client.Authenticator = OAuth1Authenticator.ForAccessToken(consumerKey, consumerSecret, requestToken.UserToken, requestToken.UserSecret,verifier);
-            
-
-            var request = new RestRequest(ACCESS_TOKEN_URL, Method.GET);
-            var response = client.Execute(request);
-
-            return GetTokenFromParams(response.Content);
-
-        }
-
         #endregion
 
         #region user methods
@@ -207,13 +144,7 @@ namespace TumblDotNet
         }
 
         #endregion
-
     }
 
-    public class OAuthRequestTokenResponse
-    {
-        public string oauth_token { get; set; }
-        public string oauth_token_secret { get; set; }
-        public bool callback_confirmed { get; set; }
-    }
+
 }
